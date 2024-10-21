3432a88185971cb8b2816c6e8feeb9f14f1ab137
Dac Long <daclong.k17@gmail.com>
2024-10-21 12:51:42 +0700

    email verification

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,9 +2,20 @@ const UserModel = require("../src/models/userModel");
 const bcrypt = require("bcrypt");
 const asyncHandle = require("express-async-handler");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: process.env.USERNAME_EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
 
 const getJsonWebToken = async (email, id) => {
-  console.log(email, id);
   const payload = {
     email,
     id,
@@ -16,12 +27,47 @@ const getJsonWebToken = async (email, id) => {
   return token;
 };
 
+const handleSendMail = async (val, email) => {
+  try {
+    await transporter.sendMail({
+      from: `Support Shoes Application <${process.env.USERNAME_EMAIL}>`,
+      to: email,
+      subject: "Verification email code",
+      text: "Your code to verification email",
+      html: `<h1>${val}</h1>`,
+    });
+
+    return "OK";
+  } catch (error) {
+    return error;
+  }
+};
+
+const verification = asyncHandle(async (req, res) => {
+  const { email } = req.body;
+
+  const verificationCode = Math.round(1000 + Math.random() * 9000);
+
+  try {
+    await handleSendMail(verificationCode, email);
+    res.status(200).json({
+      mess: "Send verification code successfully",
+      data: {
+        code: verificationCode,
+      },
+    });
+  } catch (error) {
+    res.status(401);
+    throw new Error("Can not send email");
+  }
+});
+
 const register = asyncHandle(async (req, res) => {
   const { email, username, password } = req.body;
 
   const existingUser = await UserModel.findOne({ email });
 
-  console.log(existingUser);
+  // console.log(existingUser);
 
   if (existingUser) {
     res.status(401);
@@ -44,11 +90,43 @@ const register = asyncHandle(async (req, res) => {
     data: {
       email: newUser.email,
       id: newUser.id,
-      accessToken: await getJsonWebToken(email, newUser.id),
+      accesstoken: await getJsonWebToken(email, newUser.id),
+    },
+  });
+});
+
+const login = asyncHandle(async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await UserModel.findOne({ email });
+
+  if (!existingUser) {
+    res.status(403).json({
+      mess: "User not found!",
+    });
+
+    throw new Error("User not found!");
+  }
+
+  const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+
+  if (!isMatchPassword) {
+    res.status(401);
+    throw new Error("Email or Password is not corret!");
+  }
+
+  res.status(200).json({
+    mess: "Login successfully",
+    data: {
+      id: existingUser.id,
+      email: existingUser.email,
+      accesstoken: await getJsonWebToken(email, existingUser.id),
     },
   });
 });
 
 module.exports = {
   register,
+  login,
+  verification,
 };
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,8 +1,14 @@
 const Router = require("express");
-const { register } = require("../../controllers/auth.controller");
+const {
+  register,
+  login,
+  verification,
+} = require("../../controllers/auth.controller");
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/verification", verification);
 
 module.exports = authRouter;
